eb8bf5bfadff9e5e7929f7916855197b6bd04319
quoccuonglqd <quoccuonglqd123@gmail.com>
2021-08-17 00:14:59 +0700

    add region-based loss

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -4,10 +4,11 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
 from .dice_loss import DiceLoss
 from .lovasz_loss import LovaszLoss
 from .focal_loss import FocalLoss
+from .regionloss import *
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss', 'FocalLoss',
-    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss', 'DiceLoss'
+    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss', 'DiceLoss','SSLoss'
 ]
